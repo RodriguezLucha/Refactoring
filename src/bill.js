5f4354f7b5909e41e8cf6e10193f4a86405e05e1
Rudy <RodriguezLucha@gmail.com>
2019-07-14 13:46:40 -0700

    Removed invoices variable

--- src/bill.js
+++ src/bill.js
@@ -1,27 +1,32 @@
 function statement (invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+
+  return renderPlainText(statementData, plays);
 }
 
 export default statement;
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
-  for(let perf of invoice.performances) {
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+  for(let perf of data.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
   function totalAmount() {
     let result = 0;
-    for(let perf of invoice.performances) {
+    for(let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
   }
   function totalVolumeCredits() {
     let volumeCredits = 0;
-    for(let perf of invoice.performances) {
+    for(let perf of data.performances) {
       volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
