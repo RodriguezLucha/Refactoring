5dba52656cd814d1d2b2235c3f481b92c993bdd2
Rudy <RodriguezLucha@gmail.com>
2019-07-14 14:04:59 -0700

    Move volumnCredits up the heirarchy

--- src/bill.js
+++ src/bill.js
@@ -9,6 +9,7 @@ function statement (invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
   function playFor(aPerformance){
@@ -35,6 +36,13 @@ function statement (invoice, plays) {
     }
     return result;
   }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if('comedy' === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 export default statement;
@@ -58,7 +66,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for(let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += perf.volumeCredits;
     }
     return volumeCredits;
   }
@@ -67,12 +75,5 @@ function renderPlainText(data) {
       style: 'currency', currency: 'USD',
       minimumFractionDigits: 2
     }).format(aNumber / 100);
-  }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
