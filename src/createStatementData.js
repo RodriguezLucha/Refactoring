937ae28b7db1b295ce1269ea994f0b166341107a
Rudy <RodriguezLucha@gmail.com>
2019-07-15 15:18:55 -0700

    Add tombstone to super class amount method

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -34,21 +34,7 @@ class PerformanceCalculator {
     this.play = aPlay;
   }
   get amount() {
-    let result = 0;
-    switch(this.play.type) {
-    case 'tragedy':
-      throw 'bad thing';
-    case 'comedy':
-      result = 30000;
-      if(this.performance.audience > 20) {
-        result += 10000 + 500 * (this.performance.audience - 20);
-      }
-      result += 300 * this.performance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error('subclass responsibility');
   }
   get volumeCredits() {
     let result = 0;
@@ -78,5 +64,12 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 class ComedyCalculator extends PerformanceCalculator {
-
+  get amount() {
+    let result = 30000;
+    if(this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
