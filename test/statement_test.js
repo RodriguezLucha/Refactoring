87caf11e3a5a561b17a4dd150cc118216fec5c87
Rudy <RodriguezLucha@gmail.com>
2019-07-15 14:23:39 -0700

    Refactor tests

--- test/statement_test.js
+++ test/statement_test.js
@@ -11,25 +11,29 @@ let minify = require('html-minifier').minify;
 
 import {statement, htmlStatement} from '../src/statement';
 describe('Bill', () => {
+  let invoice = null;
+  let plays = null;
+
+  beforeEach('Read in the invoice and plays objects', async()=>{
+    invoice = await readInJSON('data/invoice.json');
+    plays = await readInJSON('data/plays.json');
+  });
+
   it('returns a text bill string', async () => {
-    let invoice = await readFile('data/invoice.json', 'utf8');
-    let plays = await readFile('data/plays.json', 'utf8');
-    invoice = JSON.parse(invoice);
-    plays = JSON.parse(plays);
     let actual = statement(invoice, plays);
     let expected = await readFile('data/expected.txt', 'utf8');
     expect(actual).to.be.equal(expected);
   });
   it('returns a html bill string', async () => {
-    let invoice = await readFile('data/invoice.json', 'utf8');
-    let plays = await readFile('data/plays.json', 'utf8');
-    invoice = JSON.parse(invoice);
-    plays = JSON.parse(plays);
     let actual = htmlStatement(invoice, plays);
     let expected = await readFile('data/expected.html', 'utf8');
     expect(minifyHtml(actual)).to.be.equal(minifyHtml(expected));
   });
 });
+
+async function readInJSON(invoice) {
+  return JSON.parse(await readFile(invoice, 'utf8'));
+}
 
 function minifyHtml(str){
   minify(str, {
